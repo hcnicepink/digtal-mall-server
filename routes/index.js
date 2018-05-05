f952feff13e11adb69dbd7bc0b6e1eca95a263b3
韩潮 <hcnicepink@163.com>
2018-05-05 15:57:23 +0800

    初始化服务端

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.json({
+    "name": "Jack",
+    "age": "10"
+  })
+});
+
+module.exports = router;
