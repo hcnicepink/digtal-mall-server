6954984913af2581875635d8a16bd188ed5ed5f9
韩潮 <hcnicepink@163.com>
2018-05-28 16:17:41 +0800

    增加头像上传

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,9 @@ let router = express.Router()
 let User = require('../models/user')
 let crypto = require('crypto')
 let Buffer = require('buffer')
+let Busboy = require('busboy')
+let path = require('path')
+let fs = require('fs')
 let checkToken = require('../util/checkToken')
 const salt = 'hellodigtalmall'
 const alg = 'sha512'
@@ -152,5 +155,34 @@ router.get('/checkLogin', (req, res, next) => {
       result: checkResult
     })
   }
+})
+
+router.post('/uploadAvatar', (req, res, next) => {
+  let busboy = new Busboy({ headers: req.headers })
+  busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+    console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype)
+
+    let saveTo = path.join(__dirname, '../public/images/avatar',path.basename(fieldname))
+    file.pipe(fs.createWriteStream(saveTo))
+
+    file.on('data', function(data) {
+      console.log('File [' + fieldname + '] got ' + data.length + ' bytes')
+    })
+    file.on('end', function() {
+      console.log('File [' + fieldname + '] Finished');
+    })
+  })
+  busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+    console.log('Field [' + fieldname + ']: value: ' + inspect(val))
+  })
+  busboy.on('finish', function() {
+    console.log('Done parsing form!')
+    res.json({
+      code: 200,
+      msg: 'good boy',
+      result: null
+    })
+  });
+  req.pipe(busboy)
 })
 module.exports = router
